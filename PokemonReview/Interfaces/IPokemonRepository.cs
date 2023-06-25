51d20167985076cbe031e291ca190c17d48bcf0d
charles <cobas050@uottawa.ca>
2023-06-25 01:16:28 -0400

    MVC pattern

--- PokemonReview/Interfaces/IPokemonRepository.cs
+++ PokemonReview/Interfaces/IPokemonRepository.cs
@@ -5,5 +5,10 @@ namespace PokemonReview.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        bool PokemonExists(int pokeId);
+        decimal GetPokemonRating(int pokeId);
+
     }
 }
