132dd9226f069bd3ac40a8d996b59c13b3316654
charles <cobas050@uottawa.ca>
2023-06-25 23:41:08 -0400

    Add Country MVC

---
+++ PokemonReview/Interfaces/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReview.Models;
+
+namespace PokemonReview.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        bool CountryExists(int id);
+        ICollection<Owner> GetOwnersFromAcountry(int countryId);
+    }
+}
