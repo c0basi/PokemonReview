1eae91c6034bec68e926ac769cbbde1288dd9caa
charles <cobas050@uottawa.ca>
2023-06-26 01:11:40 -0400

    catwegory post

--- PokemonReview/Controllers/CategoryController.cs
+++ PokemonReview/Controllers/CategoryController.cs
@@ -59,5 +59,32 @@ namespace PokemonReview.Controllers
             }
             return Ok(pokemons);
         }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateCategory([FromBody] CategoryDto categoryCreate)
+        {
+            if (categoryCreate == null)
+                return BadRequest(ModelState);
+            var existingCategory = _categoryRepository.GetCategories().Where(c => c.Name.Trim().ToUpper() == categoryCreate.Name.TrimEnd().ToUpper()).FirstOrDefault();
+
+            if(existingCategory != null)
+            {
+                ModelState.AddModelError("", "Category Already Exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var _categoryMap = _mapper.Map<Category>(categoryCreate);
+            if (!_categoryRepository.CreateCategory(_categoryMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+            return Ok("Successfully Created");
+        }
     }
 }
--- PokemonReview/Interfaces/ICategoryRepository.cs
+++ PokemonReview/Interfaces/ICategoryRepository.cs
@@ -8,6 +8,8 @@ namespace PokemonReview.Interfaces
         Category GetCategory(int id);
         ICollection<Pokemon> GetPokemonByCategory(int categoryId);
         bool CategoryExists(int id);
+        bool CreateCategory(Category category);
+        bool Save();
 
     }
 }
